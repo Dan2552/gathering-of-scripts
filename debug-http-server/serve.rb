02439e9bccb480413db38f045f94421b03410f3a
Daniel Inkpen <dan2552@gmail.com>
2018-01-30 17:51:29 +0000

    Rename serve to debug-http-server

--- debug-http-server/serve.rb
+++ debug-http-server/serve.rb
@@ -3,6 +3,14 @@
 require 'pry'
 require 'sinatra'
 
+set :bind, '0.0.0.0'
+
+helpers do
+  def request_headers
+    env.inject({}){|acc, (k,v)| acc[$1.downcase] = v if k =~ /^http_(.*)/i; acc}
+  end
+end
+
 get '/*' do
   binding.pry
 end
