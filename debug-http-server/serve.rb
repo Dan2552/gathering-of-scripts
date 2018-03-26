cf66a2d05b35194d16dbde10ccc1c22997f6f718
Daniel Inkpen <dan2552@gmail.com>
2018-03-26 17:33:33 +0100

    Update debug-http-server

--- debug-http-server/serve.rb
+++ debug-http-server/serve.rb
@@ -9,20 +9,42 @@ helpers do
   def request_headers
     env.inject({}){|acc, (k,v)| acc[$1.downcase] = v if k =~ /^http_(.*)/i; acc}
   end
+
+  def request_body
+    request.body.string
+  end
+
+  def endpoint
+    "#{env["REQUEST_METHOD"]} #{request.url}"
+  end
+
+  def debugger
+    puts "=== endpoint"
+    puts endpoint
+    puts "=== headers"
+    puts request_headers
+    puts "=== body"
+    puts request_body
+    binding.pry
+  end
 end
 
 get '/*' do
-  binding.pry
+  debugger
 end
 
 post '/*' do
-  binding.pry
+  debugger
 end
 
 patch '/*' do
-  binding.pry
+  debugger
+end
+
+put '/*' do
+  debugger
 end
 
 delete '/*' do
-  binding.pry
+  debugger
 end
